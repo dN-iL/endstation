e1e02c73b6ce95f835123fce1a1347e7505e37a9
Daniel Benner <daniel.benner@tum.de>
2016-11-26 08:48:05 +0100

    refine segue counting

--- test/9-Florian/analyzer.js
+++ test/9-Florian/analyzer.js
@@ -1,74 +1,111 @@
 var fs = require('fs');
 var path = require('path');
-var content = "1479134652.2156,startScenarioBlock,stdCE-37693#\
-1479134654.401183,viewUpdate,Critical,Andreas,CEList#\
-1479134659.618824,segue,ParticipantOverview,,DetailView,Andreas,OverviewTable#\
-1479134659.684228,segue,unknown,,DetailView,current,Navigation#\
-1479134664.980276,retreat,Andreas#\
-1479134664.999433,viewUpdate,Retreat,Andreas,CEList#\
-1479134667.253171,segue,unknown,,ParticipantOverview,,Navigation#\
-1479134678.21493,startScenarioBlock,stdWarning-33261#\
-1479134680.61079,viewUpdate,Warning,Simon,CEList#\
-1479134683.21723,endScenarioBlock,stdCE-37693#\
-1479134689.931963,segue,ParticipantOverview,,DetailView,Simon,OverviewTable#\
-1479134689.956243,segue,unknown,,DetailView,current,Navigation#\
-1479134709.2161,endScenarioBlock,stdWarning-33261#\
-1479134712.491806,segue,unknown,,ParticipantOverview,,Navigation#\
-1479134714.21803,startScenarioBlock,stdWarning-39727#\
-1479134716.31852,viewUpdate,Warning,SÃ¶nke,CEList#\
-1479134734.003526,segue,ParticipantOverview,,DetailView,SÃ¶nke,OverviewTable#\
-1479134734.038929,segue,unknown,,DetailView,current,Navigation#\
-1479134745.21929,endScenarioBlock,stdWarning-39727#\
-1479134750.21859,startScenarioBlock,stdCE-32631#\
-1479134752.339621,viewUpdate,Critical,Thorben,CEList#\
-1479134754.248146,segue,unknown,,ParticipantOverview,,Navigation#\
-1479134776.21967,startScenarioBlock,coreTempPeak-87748#\
-1479134778.376531,viewUpdate,Critical,Martin,CEList#\
-1479134781.22047,endScenarioBlock,stdCE-32631#\
-1479134798.371393,segue,ParticipantOverview,current,DetailView,Martin,CEList#\
-1479134798.401687,segue,unknown,,DetailView,current,Navigation#\
-1479134807.096127,segue,DetailView,current,DetailView,Thorben,CEList#\
-1479134809.2212,endScenarioBlock,coreTempPeak-87748#\
-1479134814.21998,startScenarioBlock,stdWarning-2862#\
-1479134814.85909,segue,unknown,,ParticipantOverview,,Navigation#\
-1479134816.408389,viewUpdate,Warning,Martin,CEList#\
-1479134838.141705,segue,ParticipantOverview,,DetailView,Martin,OverviewTable#\
-1479134838.186481,segue,unknown,,DetailView,current,Navigation#\
-1479134845.22094,endScenarioBlock,stdWarning-2862#\
-1479134849.82604,segue,DetailView,current,DetailView,Martin,CEList#\
-1479134850.21952,startScenarioBlock,importantVsUnimportant-34270#\
-1479134851.210598,segue,DetailView,current,DetailView,Martin,CEList#\
-1479134852.625563,viewUpdate,Critical,SÃ¶nke,CEList#\
-1479134853.365996,viewUpdate,Warning,SÃ¶nke,CEList#\
-1479134855.931142,segue,HeartRateGraph,Martin,HumidityGraph,Martin,Button#\
-1479134856.352332,viewUpdate,Critical,Martin,CEList#\
-1479134860.885117,segue,unknown,,ParticipantOverview,,Navigation#\
-1479134864.807927,segue,ParticipantOverview,,DetailView,Martin,OverviewTable#\
-1479134864.842343,segue,unknown,,DetailView,current,Navigation#\
-1479134867.503427,retreat,Martin#\
-1479134867.530636,viewUpdate,Retreat,Martin,CEList#\
-1479134870.083753,segue,unknown,,ParticipantOverview,,Navigation#\
-1479134885.22088,endScenarioBlock,importantVsUnimportant-34270#\
-1479134890.22188,startScenarioBlock,stdWarning-17809#\
-1479134892.459691,viewUpdate,Warning,Johannes,CEList#\
-1479134899.053065,segue,ParticipantOverview,,DetailView,Johannes,OverviewTable#\
-1479134899.092306,segue,unknown,,DetailView,current,Navigation#\
-1479134914.545359,segue,DetailView,current,DetailView,SÃ¶nke,CEList#\
-1479134921.22281,endScenarioBlock,stdWarning-17809#\
-1479134922.086324,segue,DetailView,current,DetailView,Martin,CEList#\
-1479134925.039551,segue,unknown,,ParticipantOverview,,Navigation#\
-1479134926.22014,startScenarioBlock,noConnection-10014#\
-1479134928.503561,viewUpdate,Critical,Simon,CEList#\
-1479134928.555136,viewUpdate,Critical,Simon,CEList#\
-1479134928.599298,viewUpdate,Critical,Simon,CEList#\
-1479134928.635244,viewUpdate,Critical,Simon,CEList#\
-1479134928.670201,viewUpdate,Critical,Simon,CEList#\
-1479134935.66731,segue,ParticipantOverview,,DetailView,Simon,OverviewTable#\
-1479134935.720155,segue,unknown,,DetailView,current,Navigation#\
-1479134946.772087,retreat,Simon#\
-1479134946.779707,viewUpdate,Retreat,Simon,CEList#\
-1479134957.22314,endScenarioBlock,noConnection-10014#\
-1479134973.25899,segue,unknown,,ParticipantOverview,,Navigation"
+var content = "1479301434.16726,startScenarioBlock,stdCE-76696#\
+1479301434.436052,viewUpdate,Critical,SÃ¶nke,CEList#\
+1479301436.811988,segue,ParticipantOverview,current,DetailView,SÃ¶nke,CEList#\
+1479301436.883465,segue,unknown,,DetailView,current,Navigation#\
+1479301440.690075,retreat,SÃ¶nke#\
+1479301440.704783,viewUpdate,Retreat,SÃ¶nke,CEList#\
+1479301443.677289,segue,unknown,,ParticipantOverview,,Navigation#\
+1479301460.16652,startScenarioBlock,stdWarning-57440#\
+1479301460.567484,viewUpdate,Warning,Martin,CEList#\
+1479301463.27318,segue,ParticipantOverview,,DetailView,Martin,OverviewTable#\
+1479301463.315061,segue,unknown,,DetailView,current,Navigation#\
+1479301465.16681,endScenarioBlock,stdCE-76696#\
+1479301472.011855,segue,unknown,,ParticipantOverview,,Navigation#\
+1479301479.665485,segue,ParticipantOverview,,DetailView,Martin,OverviewTable#\
+1479301479.695957,segue,unknown,,DetailView,current,Navigation#\
+1479301483.747699,segue,unknown,,ParticipantOverview,,Navigation#\
+1479301491.16774,endScenarioBlock,stdWarning-57440#\
+1479301496.16912,startScenarioBlock,stdWarning-88925#\
+1479301496.481971,viewUpdate,Warning,Martin,CEList#\
+1479301503.140997,segue,ParticipantOverview,,DetailView,Martin,OverviewTable#\
+1479301503.168036,segue,unknown,,DetailView,current,Navigation#\
+1479301521.803856,segue,unknown,,ParticipantOverview,,Navigation#\
+1479301527.16782,endScenarioBlock,stdWarning-88925#\
+1479301532.16737,startScenarioBlock,stdCE-8063#\
+1479301532.759479,viewUpdate,Critical,Thorben,CEList#\
+1479301535.067803,segue,ParticipantOverview,current,DetailView,Thorben,CEList#\
+1479301535.102867,segue,unknown,,DetailView,current,Navigation#\
+1479301538.002273,segue,HeartRateGraph,Thorben,HumidityGraph,Thorben,Button#\
+1479301543.196862,retreat,Thorben#\
+1479301543.312564,viewUpdate,Retreat,Thorben,CEList#\
+1479301544.780173,segue,unknown,,ParticipantOverview,,Navigation#\
+1479301556.634962,segue,ParticipantOverview,current,DetailView,Martin,CEList#\
+1479301556.665676,segue,unknown,,DetailView,current,Navigation#\
+1479301558.16599,startScenarioBlock,coreTempPeak-41166#\
+1479301558.290824,segue,unknown,,ParticipantOverview,,Navigation#\
+1479301559.24995,viewUpdate,Critical,Johannes,CEList#\
+1479301563.17037,endScenarioBlock,stdCE-8063#\
+1479301571.661559,segue,ParticipantOverview,current,DetailView,Johannes,CEList#\
+1479301571.698733,segue,unknown,,DetailView,current,Navigation#\
+1479301575.218301,segue,HeartRateGraph,Johannes,CoreTempGraph,Johannes,Button#\
+1479301577.656047,segue,unknown,,ParticipantOverview,,Navigation#\
+1479301580.546788,segue,ParticipantOverview,current,DetailView,Johannes,CEList#\
+1479301580.589845,segue,unknown,,DetailView,current,Navigation#\
+1479301582.734959,segue,HeartRateGraph,Johannes,WristTempGraph,Johannes,Button#\
+1479301583.001842,segue,WristTempGraph,Johannes,HumidityGraph,Johannes,Button#\
+1479301584.552209,segue,HumidityGraph,Johannes,CoreTempGraph,Johannes,Button#\
+1479301586.856977,retreat,Johannes#\
+1479301586.932012,viewUpdate,Retreat,Johannes,CEList#\
+1479301587.770141,segue,unknown,,ParticipantOverview,,Navigation#\
+1479301591.16914,endScenarioBlock,coreTempPeak-41166#\
+1479301596.16967,startScenarioBlock,stdWarning-94513#\
+1479301597.914346,viewUpdate,Warning,Martin,CEList#\
+1479301600.07998,segue,ParticipantOverview,current,DetailView,Martin,CEList#\
+1479301600.117132,segue,unknown,,DetailView,current,Navigation#\
+1479301610.515694,segue,unknown,,ParticipantOverview,,Navigation#\
+1479301613.132038,segue,ParticipantOverview,current,DetailView,Martin,CEList#\
+1479301613.174358,segue,unknown,,DetailView,current,Navigation#\
+1479301615.31342,segue,DetailView,current,DetailView,Martin,SmallOverview#\
+1479301615.975749,segue,DetailView,current,DetailView,Martin,SmallOverview#\
+1479301617.14677,segue,unknown,,ParticipantOverview,,Navigation#\
+1479301621.857209,segue,ParticipantOverview,,DetailView,Martin,OverviewTable#\
+1479301621.89805,segue,unknown,,DetailView,current,Navigation#\
+1479301625.14007,segue,unknown,,ParticipantOverview,,Navigation#\
+1479301627.17056,endScenarioBlock,stdWarning-94513#\
+1479301632.1745,startScenarioBlock,importantVsUnimportant-71507#\
+1479301632.546207,viewUpdate,Critical,Johannes,CEList#\
+1479301633.970351,viewUpdate,Warning,Johannes,CEList#\
+1479301635.796016,segue,ParticipantOverview,current,DetailView,Johannes,CEList#\
+1479301635.822173,segue,unknown,,DetailView,current,Navigation#\
+1479301637.628137,viewUpdate,Critical,Andreas,CEList#\
+1479301638.145104,segue,HeartRateGraph,Johannes,HumidityGraph,Johannes,Button#\
+1479301640.197355,retreat,Johannes#\
+1479301640.632847,viewUpdate,Retreat,Johannes,CEList#\
+1479301641.782402,segue,unknown,,ParticipantOverview,,Navigation#\
+1479301643.722393,segue,ParticipantOverview,current,DetailView,Johannes,CEList#\
+1479301643.754399,segue,unknown,,DetailView,current,Navigation#\
+1479301646.087042,segue,HeartRateGraph,Johannes,WristTempGraph,Johannes,Button#\
+1479301654.732357,segue,unknown,,ParticipantOverview,,Navigation#\
+1479301667.17049,endScenarioBlock,importantVsUnimportant-71507#\
+1479301672.17318,startScenarioBlock,stdWarning-7352#\
+1479301672.823992,viewUpdate,Warning,Lorenzo,CEList#\
+1479301674.732295,segue,ParticipantOverview,current,DetailView,Lorenzo,CEList#\
+1479301674.768565,segue,unknown,,DetailView,current,Navigation#\
+1479301683.065806,segue,unknown,,ParticipantOverview,,Navigation#\
+1479301703.17893,endScenarioBlock,stdWarning-7352#\
+1479301708.17571,startScenarioBlock,noConnection-42485#\
+1479301708.532707,viewUpdate,Critical,Lorenzo,CEList#\
+1479301708.575021,viewUpdate,Critical,Lorenzo,CEList#\
+1479301708.617667,viewUpdate,Critical,Lorenzo,CEList#\
+1479301708.651737,viewUpdate,Critical,Lorenzo,CEList#\
+1479301708.694052,viewUpdate,Critical,Lorenzo,CEList#\
+1479301711.033557,segue,ParticipantOverview,current,DetailView,Lorenzo,CEList#\
+1479301711.064713,segue,unknown,,DetailView,current,Navigation#\
+1479301713.77676,segue,unknown,,ParticipantOverview,,Navigation#\
+1479301717.051496,segue,ParticipantOverview,,DetailView,Lorenzo,OverviewTable#\
+1479301717.08362,segue,unknown,,DetailView,current,Navigation#\
+1479301720.369294,retreat,Lorenzo#\
+1479301720.450088,viewUpdate,Retreat,Lorenzo,CEList#\
+1479301725.504476,segue,unknown,,ParticipantOverview,,Navigation#\
+1479301727.434864,segue,ParticipantOverview,,DetailView,Lorenzo,OverviewTable#\
+1479301727.465281,segue,unknown,,DetailView,current,Navigation#\
+1479301729.512743,segue,unknown,,ParticipantOverview,,Navigation#\
+1479301739.17245,endScenarioBlock,noConnection-42485#\
+1479301738.13509,segue,ParticipantOverview,current,DetailView,Lorenzo,CEList#\
+1479301738.168174,segue,unknown,,DetailView,current,Navigation#\
+1479301740.372233,segue,unknown,,ParticipantOverview,,Navigation"
 
 var mapper = function(toBeMapped) {
   switch(toBeMapped) {
@@ -85,14 +122,16 @@ var analyzeComponentTest = function(content) {
   var logs = content.split('#');
   
   /* WHAT IS RECORDED FOR EACH SCENARIO BLOCK?
+    ==========================================
   stdCE - id - person - start - onDetailView - retreat
-  stdWarning - nichts, evtl. +1 bei retreat fehler
+  stdWarning - nichts
   coreTempPeak - person - start - onDetailView - onGraph - retreat
   noConnection - person - start - onDetailView - retreat
   importantVsUnimportant - unimportanPerson - importantPerson - start - onDetailViewUnimportant - onDetailViewImportant - retreatUnimportant - retreatImportant
   */
 
   /* WHAT IS RECORDED OVERALL
+    =========================
   # wrong retreats
   # right retreats
   # clicks while task open
@@ -104,7 +143,11 @@ var analyzeComponentTest = function(content) {
   # clicks on medical card
   */
 
+  // array that is returned with the data of the component test
+  // each scenario block is appended to the array as an object containing its data
   var scenarioBlockData = [];
+
+  // overall measurements
   var wrongRetreats = 0;
   var rightRetreats = 0;
   var clicksWhileTaskOpen = 0;
@@ -116,9 +159,12 @@ var analyzeComponentTest = function(content) {
   var clicksOnMedicalCard = 0;
   var overallSegues = 0;
 
-
   // helper for assigning view updates to scenario blocks
   var viewUpdateQueue = [];
+  // helper for identifying duplicate segues
+  var lastSegueOn = 0;
+
+  // look at each log and fill the scenarioBlockData object accordingly
   logs.forEach(function(logString) {
     log = logString.split(',');
     var timestamp = log[0];
@@ -127,7 +173,7 @@ var analyzeComponentTest = function(content) {
         var type = log[2].split('-')[0];
         if(type == 'stdWarning') break;
         var id = log[2].split('-')[1];
-        scenarioBlockData.push({'type': type, 'id': id, 'active': true});
+        scenarioBlockData.push({'type': type, 'id': id, 'clicksWhileActive': 0, 'active': true});
         viewUpdateQueue.push(id);
         // importantVsUnimportant awaits 3 viewUpdates
         if(type == 'importantVsUnimportant') {
@@ -165,11 +211,13 @@ var analyzeComponentTest = function(content) {
         });
         break;
       case 'segue':
+        //ignore duplicate segues
+        if(parseFloat(timestamp) - parseFloat(lastSegueOn) < 0.15) break;
         var toPerson = log[5];
         var via = log[6];
+        // check if this is a reaction to a cricital event
         if(log[4] == 'DetailView') {
           scenarioBlockData.forEach(function(blockData) {
-            // check if this is a reaction to a critical event
             if(blockData['type'] == 'importantVsUnimportant') {
               if(!blockData.hasOwnProperty('onDetailViewUnimportant') && 
                 blockData.hasOwnProperty('unimportanPerson') && 
@@ -186,9 +234,35 @@ var analyzeComponentTest = function(content) {
               blockData['onDetailView'] = timestamp;
             }
           });
-        }
-        // increment counters
+        } else if(/Graph$/.test(log[4])) {
+          changesToAnotherGraph++;
+          if(log[4] == 'CoreTempGraph') {
+            scenarioBlockData.forEach(function(blockData) {
+              if(blockData['active'] == true && 
+                blockData['type'] == 'coreTempPeak' && 
+                blockData['person'] == toPerson &&
+                !blockData.hasOwnProperty('onGraph')) {
+                blockData['onGraph'] = timestamp;
+              }
+            });
+          }
+        } 
+
+        // increment segue counters
+
         overallSegues++;
+        // clicksWhileActive counter for every active block
+        scenarioBlockData.forEach(function(blockData) {
+          if(blockData['active'] == true) {
+            if((blockData['type'] == 'importantVsUnimportant' && 
+                !blockData.hasOwnProperty['retreatImportant'] &&
+                !blockData.hasOwnProperty['retreatUnimportant']) ||
+              !blockData.hasOwnProperty['retreat']) {
+              blockData['clicksWhileActive'] += 1;
+            }
+          }
+        });
+        // navigation counters
         switch(via) {
           case 'CEList':
             clicksOnCEList++;
@@ -205,17 +279,10 @@ var analyzeComponentTest = function(content) {
           case 'OverviewTable':
             clicksOnTableViewCell++;
             break;
-          case 'HeartrateGraph':
-          case 'StressLevelGraph':
-          case 'CoreTempGraph':
-          case 'AnkleTempGraph':
-          case 'WristTempGraph':
-          case 'HumidityGraph':
-            changesToAnotherGraph++;
-            break;
           default:
             break;
         }
+        lastSegueOn = timestamp;
         break;
       case 'retreat':
         var person = log[2];
@@ -249,7 +316,7 @@ var analyzeComponentTest = function(content) {
         break;
     }
   });
-  scenarioBlockData.push({'numbers': [rightRetreats, wrongRetreats, clicksOnCEList, clicksOnStatusWidget, clicksOnTableViewCell]});
+  scenarioBlockData.push({'numbers': [rightRetreats, wrongRetreats, clicksOnCEList, clicksOnStatusWidget, clicksOnTableViewCell, changesToAnotherGraph, overallSegues]});
   console.log(scenarioBlockData);
   return scenarioBlockData;
 }

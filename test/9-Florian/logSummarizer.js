c3a8ac9258d2cb85fc3d4a34129db157d5b2b074
Daniel Benner <daniel.benner@tum.de>
2016-12-01 12:55:00 +0100

    implement a converter that transforms the json summary to a convertible format

--- test/9-Florian/logSummarizer.js
+++ test/9-Florian/logSummarizer.js
@@ -20,7 +20,7 @@ var allMatchingItems = function(inFolder, matchFunction) {
 var getTest = function(testNumber) {
   switch (testNumber) {
     case '2':
-      return 'compontentTest'
+      return 'componentTest'
       break;
     case '3':
       return 'speedTest-1'
@@ -37,8 +37,9 @@ var getTest = function(testNumber) {
   }
 }
 
+// looks in all participant folder for all log files, analyses them and returns the whole analysis in an array
 var summarizeLogs = function() {
-  var summary = {};
+  var summary = [];
   var participantFolders = allMatchingItems('.', matchParticipantFolder);
   participantFolders.forEach(function(participantFolder) {
     var participant = participantFolder.split('-')[1];
@@ -54,10 +55,89 @@ var summarizeLogs = function() {
       var logAnalysis = analyze(fileContent);
       participantSummary[testKindOf] = logAnalysis;
     });
-    summary[participant] = participantSummary;
+    summary.push(participantSummary);
   });
-  console.log(JSON.stringify(summary));
   return summary;
 }
 
-summarizeLogs();
+var findIndexForBlock = function(blockName, test) {
+  var index;
+  switch (blockName) {
+    case 'stdCE':
+      index = 1;
+      break;
+    case 'coreTempPeak':
+      index = 2;
+      break;
+    case 'importantVsUnimportant':
+      index = 3;
+      break;
+    case 'noConnection':
+      index = 4;
+      break;
+    default:
+      index = -1;
+  }
+  return index;
+}
+
+// transforms one test of one person to csvJson
+var transformOneTestToCsvJson = function(testName, personNumber, personSummary) {
+  var person = 'person-' + personNumber;
+  var test = personSummary[testName];
+  var forCsv = [];
+  var skippedBlocks = test[test.length-1]['skipped'];
+  var blocksSkipped = skippedBlocks > 0;
+  if(blocksSkipped) {
+    skippedBlocks.forEach(function(skippedBlock) {
+      var additionToTest = {'type': skippedBlock, 'skipped': true};
+      test.splice(findIndexForBlock(skippedBlock), 0, additionToTest);
+    });
+  }
+  var boundary = test.length - 3;
+  for(var i=0; i<test.length; i++) {
+    if(i<boundary) {
+      var scenarioBlock = test[i];
+      scenarioBlock['person'] = person;
+      scenarioBlock['test'] = testName;
+      forCsv.push(scenarioBlock);
+    } else if(i==boundary) {
+      var testRetreats = test[i]['retreats'];
+      var rightRetreats = testRetreats[0];
+      var wrongRetreats = testRetreats[1];
+      var duplicateRetreats = testRetreats[2];
+      forCsv.push({'person': person, 'test': testName, 'rightRetreats': rightRetreats, 'wrongRetreats': wrongRetreats, 'duplicateRetreats': duplicateRetreats});
+    } else if(i==boundary+1) {
+      var testClicks = test[i]['clicks'];
+      var clicksOnTableView = testClicks[0];
+      var clicksOnCeList = testClicks[1];
+      var clicksOnStatusWidget = testClicks[2];
+      var clicksOnAnotherGraph = testClicks[3];
+      var clicksOverall = testClicks[4];
+      forCsv.push({'person': person, 'test': testName, 'clicksOnTableView': clicksOnTableView, 'clicksOnCeList': clicksOnCeList, 'clicksOnStatusWidget': clicksOnStatusWidget, 'clicksOnAnotherGraph': clicksOnAnotherGraph, 'clicksOverall': clicksOverall});
+    }
+    // to do: handle skipped!
+  }
+  return forCsv;
+}
+
+// transforms the analysis in a csv compatible json object that can easily be converted
+var transformToCsvJson = function() {
+  var csv = [];
+  var summary = summarizeLogs();
+  var forCsv = [];
+  summary.forEach(function(personSummary, index) {
+    forCsv.push(transformOneTestToCsvJson('componentTest', index, personSummary));
+    forCsv.push(transformOneTestToCsvJson('speedTest-1', index, personSummary));
+    forCsv.push(transformOneTestToCsvJson('speedTest-2', index, personSummary));
+    forCsv.push(transformOneTestToCsvJson('speedTest-3', index, personSummary));
+  });
+  forCsv.forEach(function(testData) {
+    testData.forEach(function(scenarioBlockData) {
+      csv.push(scenarioBlockData);
+    });
+  })
+  console.log(JSON.stringify(csv));
+}
+
+transformToCsvJson();
